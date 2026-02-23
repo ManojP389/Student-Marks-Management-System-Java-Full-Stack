76f536d537e1fc8395ee889df48b525e9181e6fa
Manoj Kumar P <manojroyal389@gmail.com>
2026-02-23 16:52:32 +0530

    Update DispData.java

--- main/java/com/lab5/DispData.java
+++ main/java/com/lab5/DispData.java
@@ -27,7 +27,7 @@ public class DispData extends HttpServlet {
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/student",
                     "root",
-                    "root123");
+                    "type_ur_dbpass");
 
             PreparedStatement ps = con.prepareStatement(
                     "select * from mark where rollno=?");
@@ -73,4 +73,5 @@ public class DispData extends HttpServlet {
 
         out.close();
     }
+
 }
